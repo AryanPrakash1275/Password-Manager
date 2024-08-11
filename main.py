533030801a0985b7b27b8e98d4b7f0c21fafd386
Aryan Prakash <aryanprakash1275@gmail.com>
2024-08-11 06:49:10 +0530

    added seaarch feature

---
+++ main.py
@@ -0,0 +1,108 @@
+from tkinter import *
+from tkinter import messagebox
+import json
+
+
+# ----------------Functions--------------------
+
+
+def generate_pass():
+    password_entry.delete(0, END)
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    password_entry.insert(0, password)
+
+
+def save():
+    website = website_entry.get()
+    email = email_Username_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def search_pass():
+    website = website_entry.get()
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email:{email}\n Password:{password}")
+
+
+# --------------------UI-----------------------
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_image)
+canvas.grid(column=1, row=0)
+
+# -----------------labels, entries and buttons----------------------
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+email_Username_label = Label(text="Email/UserName:")
+email_Username_label.grid(column=0, row=2)
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)
+website_entry.focus()
+email_Username_entry = Entry(width=39)
+email_Username_entry.grid(column=1, columnspan=2, row=2)
+email_Username_entry.insert(0, "aryanprakash1275@gmail.com")
+password_entry = Entry(width=21)
+password_entry.grid(column=1, row=3)
+gen_pass_button = Button(text="Generate Password", command=generate_pass)
+gen_pass_button.grid(column=2, row=3)
+add_button = Button(text="Add", width=32, command=save)
+add_button.grid(column=1, columnspan=2, row=4)
+search_button = Button(text="Search", command=search_pass, width=12)
+search_button.grid(column=2, row=1)
+
+window.mainloop()
